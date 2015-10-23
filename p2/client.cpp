b2047c4302463db8c800c7d3e6cbe7ae02023b64
Emil Guseynov <theluckyemil@gmail.com>
2015-10-23 19:17:45 +0300

    Update client.cpp

--- p2/client.cpp
+++ p2/client.cpp
@@ -19,19 +19,6 @@
 #define MSG_NOSIGNAL 0x2000
 #endif
 
-int set_nonblock(int fd)
-{
-    int flags;
-    #if defined(O_NONBLOCK)
-        if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
-            flags = 0;
-        return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-    #else
-        flags = 1;
-        return ioctl(fd, FIOBIO, &flags);
-    #endif
-} 
-
 int main() {
     int ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     struct sockaddr_in SockAddr;
